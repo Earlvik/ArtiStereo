d352c678f969b7d233931db6d943f0fce5b71a07
earlvik <earlviktor@gmail.com>
2014-01-30 20:53:47 +0400

    Added geometry methods for calculating the distance between points and lines, angles between lines and reflection points for the primary reflections

--- ArtiStereo/Room.cs
+++ ArtiStereo/Room.cs
@@ -5,10 +5,7 @@ using System.Text;
 
 namespace Earlvik.ArtiStereo
 {
-    public interface ILinear
-    {
-        Point GetIntersection(ILinear second);
-    }
+   
 
     [Serializable]
    public class Room
@@ -56,9 +53,12 @@ namespace Earlvik.ArtiStereo
     }
 
     [Serializable]
-   public class Line:ILinear
+   public class Line
     {
-        private const double Eps = 0.00001;
+        public Point Start { set; get; }
+        public Point End { set; get; }
+
+        private const double Eps = 0.0000001;
 
         static double VectorMultiplication(Line first, Line second)
         {
@@ -87,33 +87,33 @@ namespace Earlvik.ArtiStereo
             return (Math.Min(xs, xe) <= xp) && (xp <= Math.Max(xs, xe)) &&
                    (Math.Min(ys, ye) <= yp) && (yp <= Math.Max(ys, ye));
         }
-
-        Point SegmentIntersection(Line second)
+      
+        Point SegmentIntersection(Line second, bool wholeLine)
         {
+            //Vector multiplication between the line and the endpoint of the other line
             double d1 = Direction(second, this.Start);
             double d2 = Direction(second, this.End);
             double d3 = Direction(this, second.Start);
             double d4 = Direction(this, second.End);
-
-            if (Math.Abs(d1) < Eps && OnSegment(second, this.Start))
+            //Checking if any of the endpoints is on the other line
+            if (Math.Abs(d1) < Eps && (OnSegment(second, this.Start) || wholeLine))
             {
                 return this.Start;
             }
-            if(Math.Abs(d2)< Eps && OnSegment(second,this.End))
+            if(Math.Abs(d2)< Eps && (OnSegment(second,this.End)|| wholeLine))
             {
                 return this.End;
             }
-            if (Math.Abs(d3) < Eps && OnSegment(this, second.Start))
+            if (Math.Abs(d3) < Eps && (OnSegment(this, second.Start)|| wholeLine))
             {
                 return second.Start;
             }
-            if (Math.Abs(d4) < Eps && OnSegment(this, second.End))
+            if (Math.Abs(d4) < Eps && (OnSegment(this, second.End)|| wholeLine))
             {
                 return second.End;
             }
-
-            if ((d2*d1 < 0) && (d3*d4 < 0))
-            {
+            //Checking for intersections apart from the endpoints
+          
                 double x1 = Start.X,
                        x2 = End.X,
                        x3 = second.Start.X,
@@ -122,16 +122,130 @@ namespace Earlvik.ArtiStereo
                        y2 = End.Y,
                        y3 = second.Start.Y,
                        y4 = second.End.Y;
+            if (Math.Abs((y4 - y3)*(x2 - x1) - (x4 - x3)*(y2 - y1)) < Eps) return null;
                 double coefftA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
-               // double coefftB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
-                return new Point(x1 + coefftA*(x2-x1),y1+coefftA*(y2-y1));
+                double coefftB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+            if (wholeLine || ((d2*d1 < 0) && d3*d4 < 0))
+            {
+                return new Point(x1 + coefftA*(x2 - x1), y1 + coefftA*(y2 - y1));
             }
+         
             return null;
 
         }
 
-        public Point Start { set; get; }
-        public Point End { set; get; }
+        public static double Distance(Point a, Point b)
+        {
+            return Math.Sqrt((a.X - b.X)*(a.X - b.X) + (a.Y - b.Y)*(a.Y - b.Y));
+        } 
+
+        public static double Distance(Point point, Line line)
+        {
+            //Line equation parameters
+            double A = line.Start.Y - line.End.Y;
+            double B = line.End.X - line.Start.X;
+            double C = line.Start.X*line.End.Y - line.Start.Y*line.End.X;
+
+            double denom = Math.Sqrt(A*A + B*B);
+            if(Math.Abs(denom) < Eps) throw new Exception("the line equation is illegal");
+            return Math.Abs(A*point.X + B*point.Y + C)/denom;
+        }
+
+        public static double Angle(Line a, Line b)
+        {
+            //line equations parameters
+            double A1 = a.Start.Y - a.End.Y;
+            double B1 = a.End.X - a.Start.X;
+            //double C1 = a.Start.X*a.End.Y - a.Start.Y*a.End.X;
+
+            double A2 = b.Start.Y - b.End.Y;
+            double B2 = b.End.X - b.Start.X;
+           // double C2 = b.Start.X * b.End.Y - b.Start.Y * b.End.X;
+
+            //parallel and peprendicular lines
+            if ((Math.Abs(B1) < Eps && Math.Abs(B2) < Eps) || (Math.Abs(A1) < Eps && Math.Abs(A2) < Eps) || Math.Abs((A1 / A2) - (B1 / B2)) < Eps) return 0;
+            if (Math.Abs(A1*A2 + B1*B2) < Eps) return Math.PI/2;
+
+            //other lines
+            double denom = Math.Sqrt((A1*A1 + B1*B1)*(A2*A2 + B2*B2));
+            return Math.Abs(A1*A2 + B1*B2)/denom;
+        }
+
+        public static Point ReflectionPoint(Line wall, Point source, Point listener)
+        {
+            Line direct = new Line(source, listener);
+
+            if (wall.GetIntersection(direct, true) != null) return null; //the wall is between the listener and the source
+
+            double angle = Angle(wall, direct);
+            if (Math.Abs(angle) < Eps) //the wall is parallel with the direct line
+            {
+                return ParallelProjection(wall, new Point((source.X + listener.X)/2, (source.Y + listener.Y)/2));
+            }
+
+            Point nearPoint, farPoint;
+            double shortDist = Distance(listener,wall), longDist = Distance(source,wall);
+            if (longDist > shortDist)
+            {
+                nearPoint = listener;
+                farPoint = source;
+            }
+            else
+            {
+                nearPoint = source;
+                farPoint = listener;
+                double temp = shortDist;
+                shortDist = longDist;
+                longDist = temp;
+            }
+
+            Line farParallel;
+            if (Math.Abs(wall.Start.X - wall.End.X)<Eps)
+            {
+                farParallel = new Line(farPoint, new Point(farPoint.X, 0));
+            }
+            else
+            {
+                double wallAngleCoefft = (wall.Start.Y - wall.End.Y)/(wall.Start.X - wall.End.X);
+                Point secondPoint = new Point(0,farPoint.Y+wallAngleCoefft*(-farPoint.X));
+                farParallel = new Line(farPoint,secondPoint);
+
+            }
+
+            Point endOfFarProjection = ParallelProjection(farParallel, nearPoint);
+
+            double similarityCoefft = (shortDist)/(longDist - shortDist);
+
+            // Point on far parallel line which will be projected
+            Point farResultProjection = new Point((farPoint.X + similarityCoefft * endOfFarProjection.X) / (1 + similarityCoefft), (farPoint.Y + similarityCoefft * endOfFarProjection.Y) / (1 + similarityCoefft));
+
+            Point result = ParallelProjection(wall, farResultProjection);
+
+            return OnSegment(wall, result) ? result : null;
+
+        }
+
+        public static Point ParallelProjection(Line toLine, Point point)
+        {
+            //double A1 = fromLine.Start.Y - fromLine.End.Y;
+            //double B1 = fromLine.End.X - fromLine.Start.X;
+            //double C1 = fromLine.Start.X*fromLine.End.Y - fromLine.Start.Y*fromLine.End.X;
+
+            double A2 = toLine.Start.Y - toLine.End.Y;
+            double B2 = toLine.End.X - toLine.Start.X;
+            double C2 = toLine.Start.X * toLine.End.Y - toLine.Start.Y * toLine.End.X;
+     
+            if(Math.Abs(B2) < Eps) return new Point(-C2/A2,point.Y);
+
+            double angleCoefft = A2/(-B2);
+            //double offset = C1/(-B1);
+
+            Point start = new Point(0,point.Y+(1/angleCoefft)*point.X);
+            Point end = new Point(1,point.Y+(1/angleCoefft)*(point.X-1));
+            Line normal = new Line(start,end);
+            Point intersection =  toLine.GetIntersection(normal, true);
+            return OnSegment(toLine, intersection) ? intersection : null;
+        }
 
         public Line(Point start, Point end)
         {
@@ -145,10 +259,10 @@ namespace Earlvik.ArtiStereo
             End = new Point(xend, yend);
         }
 
-        public Point GetIntersection(ILinear second)
+        public Point GetIntersection(Line second, bool wholeLine)
         {
-            if (second is Line) return SegmentIntersection(second as Line);
-            throw new NotImplementedException("Here will be intersection for a segment and an arc");
+            if (second is Line) return SegmentIntersection(second as Line, wholeLine);
+            throw new NotImplementedException("Here will be intersection for a segment and an arc or maybe not");
         }
     }
 }
--- ArtiStereoTests/GeometryTests.cs
+++ ArtiStereoTests/GeometryTests.cs
@@ -10,14 +10,85 @@ namespace ArtiStereoTests
     [TestClass]
     public class GeometryTests
     {
-        private const double Eps = 0.0001;
+        private const double Eps = 0.0000001;
         [TestMethod]
         public void TestMethod1()
         {
             AS.Line a = new AS.Line(1,1,3,5);
             AS.Line b= new AS.Line(1,5,3,1);
-            AS.Point p = a.GetIntersection(b);
+            AS.Point p = a.GetIntersection(b,false);
             Assert.IsTrue(p!=null && Math.Abs(p.X)-2<Eps && Math.Abs(p.Y)-3<Eps,"The intersection point had to be (2,3) but was "+p);
+        }
+
+        [TestMethod]
+        public void ParallelTest()
+        {
+            AS.Line a = new AS.Line(0,2,5,2);
+            AS.Line b = new AS.Line(0,3,6,3);
+            Assert.IsNull(a.GetIntersection(b,false),"There must be no intersections, but was "+a.GetIntersection(b,false));
+        }
+
+        [TestMethod]
+        public void NonParallelNonIntersectingTest()
+        {
+            AS.Line a = new AS.Line(0,2,5,2);
+            AS.Line b = new AS.Line(7,0,7,5);
+            Assert.IsNull(a.GetIntersection(b,false),"There must be no intersections, but was "+a.GetIntersection(b,false));
+        }
+
+        [TestMethod]
+        public void NonParallelLineInterectingTest()
+        {
+            AS.Line a = new AS.Line(0, 2, 5, 2);
+            AS.Line b = new AS.Line(7, 0, 7, 5);
+            AS.Point p = a.GetIntersection(b, true);
+            Assert.IsTrue(p != null && Math.Abs(p.X) - 7 < Eps && Math.Abs(p.Y) - 2 < Eps, "The intersection point had to be (7,2) but was " + p);
+        } 
+
+        [TestMethod]
+        public void TouchingTest()
+        {
+            AS.Line a = new AS.Line(0,0,5,0);
+            AS.Line b = new AS.Line(2,0,2,5);
+            AS.Point p = a.GetIntersection(b,false);
+            Assert.IsTrue(p != null && Math.Abs(p.X - 2) < Eps && Math.Abs(p.Y) < Eps, "The intersection point had to be (2,0) but was " + p);
+        }
+
+        [TestMethod]
+        public void PointDistanceTest()
+        {
+            AS.Point a = new AS.Point(1, 1);
+            AS.Point b = new AS.Point(5,2.5);
+            Assert.IsTrue(Math.Abs(AS.Line.Distance(a,b) - Math.Sqrt(18.25)) < Eps);
+        }
+
+        [TestMethod]
+        public void AngleTest()
+        {
+            AS.Line a = new AS.Line(1,2,2,6);
+            AS.Line b = new AS.Line(2,2,6,1);
+            double angle = AS.Line.Angle(a, b);
+            Assert.IsTrue(Math.Abs(angle - Math.PI/2) <Eps,"Angle between lines had to be Pi/2, but was "+ angle);
+        }
+
+        [TestMethod]
+        public void ProjectionTest()
+        {
+           // AS.Line a = new AS.Line(1,3,4,6);
+            AS.Line b = new AS.Line(2,1,6,5);
+            AS.Point point = new AS.Point(3,5);
+            AS.Point p = AS.Line.ParallelProjection(b, point);
+            Assert.IsTrue(p!=null && Math.Abs(p.X - 4.5) <Eps && Math.Abs(p.Y - 3.5) <Eps,"Resulted projection point had to be (4.5,3.5) but was "+p );
+        }
+
+        [TestMethod]
+        public void SimpleParallelReflectionTest()
+        {
+            AS.Line wall = new AS.Line(5,0,5,10);
+            AS.Point source = new AS.Point(2,2);
+            AS.Point listener = new AS.Point(2,6);
+            AS.Point result = AS.Line.ReflectionPoint(wall, source, listener);
+            Assert.IsTrue(result!=null && Math.Abs(result.X - 5) <Eps && Math.Abs(result.Y - 4)<Eps,"The reflection point had to be (5,4), but was "+result);
         }
     }
 }
